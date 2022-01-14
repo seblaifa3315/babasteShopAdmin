76366fe36b3ebfccacda55ddc4a59a4ee37a714a
Sebastien Laifa <seblaifa@hotmail.com>
2022-01-14 10:07:06 -0800

    first commit

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import "./home.css";
+
+import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
+import Chart from "../../components/chart/Chart";
+import WidgetSm from "../../components/widgetSm/WidgetSm";
+import WidgetLg from "../../components/widgetLg/WidgetLg"
+
+import { userData } from "../../dummyData";
+
+export default function Home() {
+    return (
+        <div className="home">
+            <FeaturedInfo />
+            <Chart data={userData} title="User Analytics" grid dataKey="Active User" />
+            <div className="homeWidget">
+                <WidgetSm />
+                <WidgetLg />
+            </div>
+        </div>
+    )
+}
+
